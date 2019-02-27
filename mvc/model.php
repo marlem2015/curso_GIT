c68dffe517bf5d3f75a6d7effc5b3c8083b76497
teste Git <madrisale@gmail.com>
2019-02-27 14:04:37 -0300

    aplicação mvc - model

---
+++ mvc/model.php
@@ -0,0 +1,36 @@
+<?php
+
+class pessoa {
+
+	private $id;
+	private $nome;
+
+	public save(){
+		// lógica para salvar pessoa do banco
+
+
+	}
+
+	public update(){
+		// lógica para atualizar pessoa do banco
+
+
+	}
+
+	public remove(){
+		// lógica para remover pessoa do banco
+
+	}
+
+	public listAll(){
+		// lógica para mostrar todas as pessoas do banco
+
+
+	}
+
+}
+
+
+
+
+?>
